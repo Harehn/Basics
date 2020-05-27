77e1fa3b650e5cfff0593acac2c61a70c4cec2c9
Harehn <35270821+Harehn@users.noreply.github.com>
2020-05-27 17:26:34 -0400

    Update insertionSort.ml

--- Sorting Algorithms/insertionSort.ml
+++ Sorting Algorithms/insertionSort.ml
@@ -1,7 +1,7 @@
 exception OutOfBounds;;
 let rec randList n = if n=0 then [] else (Random.int 100)::(randList (n-1)) ;;
 let myList = randList 15 ;;
-let insert ll e = match ll with 
+let rec insert ll e = match ll with 
   |[] -> [e]
   |x::rest -> if x > e then x::(insert rest e) else e::x::rest 
 let insertionSort ll = 
