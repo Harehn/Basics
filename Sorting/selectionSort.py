0c61a812f3a6c08aa2464686b182ab30434c97c6
unknown <harehn.kaundun@mail.mcgill.ca>
2020-05-29 18:51:41 -0400

    Added insertion sort

--- Sorting/selectionSort.py
+++ Sorting/selectionSort.py
@@ -1,7 +1,8 @@
 import random
 
 
-def selectionSort(l):
+def selectionSort(ll):
+    l = ll.copy()
     toRet = []
     for i in range(len(l)):
         el = max(l)
@@ -10,6 +11,25 @@ def selectionSort(l):
     return list(reversed(toRet))
 
 
+def insertionSort(l):
+    toRet = []
+    for el in l:
+        #print(el, toRet)
+        toRet = insertElement(toRet, el)
+    return toRet
+
+def insertElement(l, e):
+    lll = l.copy()
+    for index, element in enumerate(l):
+        if element >= e:
+            lll.insert(index, e)
+            break
+    else:
+        lll.insert(len(l), e)
+    return lll
+
+
 ll = [random.randint(0, 100) for i in range(15)]
-print(ll)
-print(selectionSort(ll))
+print("unsorted", ll)
+print("selection sort", selectionSort(ll))
+print("insertion sort", insertionSort(ll))
