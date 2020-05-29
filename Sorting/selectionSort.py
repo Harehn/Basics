db03c19fb07567e1ec2d2b3a7ff23f9051733821
unknown <harehn.kaundun@mail.mcgill.ca>
2020-05-29 19:34:47 -0400

    Added bubblesort

--- Sorting/selectionSort.py
+++ Sorting/selectionSort.py
@@ -12,20 +12,28 @@ def selectionSort(ll):
 
 
 def insertionSort(l):
+    def insertElement(l, e):
+        lll = l.copy()
+        for index, element in enumerate(l):
+            if element >= e:
+                lll.insert(index, e)
+                break
+        else:
+            lll.insert(len(l), e)
+        return lll
     toRet = []
     for el in l:
         #print(el, toRet)
         toRet = insertElement(toRet, el)
     return toRet
 
-def insertElement(l, e):
-    lll = l.copy()
-    for index, element in enumerate(l):
-        if element >= e:
-            lll.insert(index, e)
-            break
-    else:
-        lll.insert(len(l), e)
+def bubbleSort(lll):
+    ll = lll.copy()
+    length = len(lll)
+    for i in range(length):
+        for j in range(i + 1, length):
+            if lll[i] >= lll[j]:
+                lll[i], lll[j] = lll[j], lll[i]
     return lll
 
 
@@ -33,3 +41,4 @@ ll = [random.randint(0, 100) for i in range(15)]
 print("unsorted", ll)
 print("selection sort", selectionSort(ll))
 print("insertion sort", insertionSort(ll))
+print("Bubble sort", bubbleSort(ll))
