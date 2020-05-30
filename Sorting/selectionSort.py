9863f9ca927fbe396b7aa8fc08aaeb62c4ecd0cb
unknown <harehn.kaundun@mail.mcgill.ca>
2020-05-29 22:08:19 -0400

    Added other sorts

--- Sorting/selectionSort.py
+++ Sorting/selectionSort.py
@@ -27,8 +27,9 @@ def insertionSort(l):
         toRet = insertElement(toRet, el)
     return toRet
 
-def bubbleSort(lll):
-    ll = lll.copy()
+
+def bubbleSort(ll):
+    lll = ll.copy()
     length = len(lll)
     for i in range(length):
         for j in range(i + 1, length):
@@ -37,8 +38,51 @@ def bubbleSort(lll):
     return lll
 
 
+def merge(l1, l2):
+    toRet = []
+    while not (len(l1) == 0) and not (len(l2) == 0):
+        if l1[0] < l2[0]:
+            toRet.append(l1[0])
+            l1 = l1[1:]
+        else:
+            toRet.append(l2[0])
+            l2 = l2[1:]
+    for n in l1:
+        toRet.append(n)
+    for n in l2:
+        toRet.append(n)
+    return toRet
+
+
+def mergeSort(ll):
+    if len(ll) < 2:
+        return ll
+    else:
+        length = len(ll)
+        a, b = ll[:length//2], ll[(length//2):]
+        c = merge(mergeSort(a), mergeSort(b))
+        return c
+
+def quickSort(ll):
+    def partition(l):
+        greater = [i for i in l[1:] if i > l[0]]
+        less = [i for i in l[1:] if i <= l[0]]
+        pivot = l[0]
+        return less, pivot, greater
+    if len(ll) < 2:
+        return ll
+    else:
+        a, b, c = partition(ll)
+        toRet = quickSort(a)
+        toRet.append(b)
+        toRet.extend(quickSort(c))
+        return toRet
+
+
 ll = [random.randint(0, 100) for i in range(15)]
 print("unsorted", ll)
 print("selection sort", selectionSort(ll))
 print("insertion sort", insertionSort(ll))
 print("Bubble sort", bubbleSort(ll))
+print("Merge sort", mergeSort(ll))
+print("Quick sort", quickSort(ll))
