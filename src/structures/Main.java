5e5e3a47d18a7913521787652760f7b2640e2210
unknown <harehn.kaundun@mail.mcgill.ca>
2020-06-27 21:27:29 -0400

    Added linked list

---
+++ src/structures/Main.java
@@ -0,0 +1,13 @@
+package structures;
+
+public class Main {
+
+	public static void main(String[] args) {
+		LinkedList l = new LinkedList();
+		for(int i = 0; i<10; i++){
+			l.insert(i);
+		}
+		l.output();
+	}
+
+}
