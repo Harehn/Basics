af069bb735877f36247469c162d874da91b480d1
unknown <harehn.kaundun@mail.mcgill.ca>
2020-07-09 17:32:44 -0400

    added variables, functions, lambdas, HOF and arithmetic #5

---
+++ Syntax.js
@@ -0,0 +1,22 @@
+function addNumbers(a, b) {
+    return a + b; ;
+}
+
+function execute(action){
+  action()
+}
+
+var integer = 12;
+var flt = 12.3;
+var a_string = "Hello";
+var char = 'm'
+var arr = [2,3,5,8];
+var bool = false;
+const PI = 3.14;
+var a = 1, b = 2, c = a + b;
+var tuples_using_arrays = ["int", 12, 12.3]
+var operations = (12 + 12 -12 )/ 12 * 2
+
+
+var lambda = function(){console.log("Higher Order Functions")};
+execute(lambda)
