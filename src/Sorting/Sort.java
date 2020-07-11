7961cd5038dde6d7ba3af34d838947df96265baf
Harehn <harehn.kaundun@mail.mcgill.ca>
2020-07-10 22:42:58 -0400

    Added BubbleSort #8

--- src/Sorting/Sort.java
+++ src/Sorting/Sort.java
@@ -19,14 +19,34 @@ public class Sort {
     return simpleArr;
   }
   
+  public static int[] bubbleSort(int[] arr0) {
+    int[] arr = cloneList(arr0);
+    for(int i = 0; i < arr.length; i++) {
+      for(int j = i; j < arr.length; j++) {
+        if(arr[i] > arr[j]) {
+          int temp = arr[j];
+          arr[j] = arr[i];
+          arr[i] = temp;
+        }
+      }
+    }
+    return arr;
+  }
+  
   public static int[] makeList(int n) {
     Random myRandom=new Random();
     int[] randomList = new int[n];
     for(int i = 0; i < n; i++){
        randomList[i] = myRandom.nextInt(100);
     }
-    System.out.println(randomList);
     return randomList;
   }
   
+  public static int[] cloneList(int[] arr0) {
+    int[] arr = new int[arr0.length];
+    for(int i = 0; i < arr.length; i++) {
+      arr[i] = arr0[i];
+    }
+    return arr;
+  }
 }
