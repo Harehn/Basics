cb4709fb9d70d3cf4975c548f2fc4ab7911dba71
unknown <harehn.kaundun@mail.mcgill.ca>
2020-07-14 21:42:58 -0400

    Added list generation and bubblesort #10

---
+++ Sorting.js
@@ -0,0 +1,25 @@
+function makeList(n){
+  var arr = [];
+  for(var i = 0; i < n; i++){
+    arr.push(Math.round(Math.random() * 100));
+  }
+  return arr;
+}
+
+function bubbleSort(arr0){
+  var arr = [...arr0]
+  for(var i = 0; i < arr.length; i++){
+    for(var j = 0; j < arr.length; j++){
+      if(arr[i] > arr[j]){
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+arr = makeList(12)
+console.log(bubbleSort(arr));
+console.log(arr)
