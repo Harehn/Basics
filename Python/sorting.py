72c6570041de1ece711034da8b6a571183452f61
Nitin <harehn.kaundun@mail.mcgill.ca>
2025-07-14 18:47:36 -0400

    Added python selection_sort

--- Python/sorting.py
+++ Python/sorting.py
@@ -5,8 +5,17 @@ def bubble_sort(arr):
                 arr[i], arr[j] = arr[j], arr[i]
     return arr
 
+def selection_sort(arr):
+    new_arr = []
+    while arr:
+        min_arr = min(arr)
+        new_arr.append(min_arr)
+        arr.remove(min_arr)
+    return new_arr
+
 import random
 length = 20
 RANGE = 10_000
 numbers = [random.randint(1, RANGE) for i in range(length)]
 print(bubble_sort(numbers.copy()))
+print(selection_sort(numbers.copy()))
